04dc50704c7580b38fd10a24c3b69e33474fcd3d
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2022-06-23 00:14:44 +0200

    add chart
    Signed-off-by: Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>

--- mySAE24/views.py
+++ mySAE24/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from .models import *
+import datetime
 def index(request):
-	query = Data.objects.all()
-	return render(request, "index.html",{"data":query})
+	if request.method == 'POST':
+		start = request.POST.get('start')
+		stop = request.POST.get('stop')
+		print(request.POST.get('capteur'))
+		if (request.POST.get('start') == ""):
+			print("NO START")
+			start = datetime.date(1800, 10, 1)
+		if (request.POST.get('stop') == ""):
+			print("NO STOP")
+			stop = (datetime.date.today()+datetime.timedelta(days=1))
+
+		print(start)
+		print(stop)
+		if request.POST.get('capteur') != "*":
+			query = Data.objects.filter(timestamp__range=(start, stop),capteur=request.POST.get('capteur'))
+		else:
+			query = Data.objects.filter(timestamp__range=(start, stop))
+		cap = Capteur.objects.all()
+		return render(request, "index.html",{"data":query,"cap":cap})
+	else:
+		query = Data.objects.all()
+		cap = Capteur.objects.all()
+		return render(request, "index.html",{"data":query,"cap":cap})
 
 

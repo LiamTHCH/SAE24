0ff0a0f4c3ecf280ba662d39c1189663090481a2
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2022-06-23 00:40:33 +0200

    add csv
    Signed-off-by: Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>

--- mySAE24/views.py
+++ mySAE24/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import *
 import datetime
+import csv
+from django.http.response import HttpResponse
+import mimetypes
 def index(request):
 	if request.method == 'POST':
+		request.session["option"] = []
 		start = request.POST.get('start')
 		stop = request.POST.get('stop')
 		print(request.POST.get('capteur'))
@@ -20,10 +24,41 @@ def index(request):
 		else:
 			query = Data.objects.filter(timestamp__range=(start, stop))
 		cap = Capteur.objects.all()
+		request.session["option"] = [start,stop,request.POST.get('capteur')]
 		return render(request, "index.html",{"data":query,"cap":cap})
 	else:
+		request.session["option"] = []
 		query = Data.objects.all()
 		cap = Capteur.objects.all()
 		return render(request, "index.html",{"data":query,"cap":cap})
 
 
+
+def csv_export(request):
+	dic = request.session["option"]
+	if len(dic) == 0:
+		dic = [datetime.date(1800, 10, 1),(datetime.date.today()+datetime.timedelta(days=1)),"*"]
+	start = dic[0]
+	stop = dic[1]
+	capteur = dic[2]
+	print(dic)
+	if capteur != "*":
+		query = Data.objects.filter(timestamp__range=(start, stop),capteur=capteur)
+	else:
+		query = Data.objects.filter(timestamp__range=(start, stop))
+
+	fieldnames = ['ID', 'ID_Capteur', 'Temperature', 'Date/Heure','nom','piece','emplacement']
+	row = []
+	for item in query:
+		row = row + [{'ID':item.id, 'ID_Capteur':item.capteur.id, 'Temperature':item.temp, 'Date/Heure':item.timestamp,'nom':item.capteur.nom,'piece':item.capteur.piece,'emplacement':item.capteur.emplacement}]
+
+	print(row)
+	with open('export.csv', 'w', encoding='UTF8', newline='') as f:
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+		writer.writeheader()
+		writer.writerows(row)
+
+	path = open('export.csv', 'r')
+	response = HttpResponse(path)
+	response['Content-Disposition'] = "attachment; filename=%s" % 'export.csv'
+	return response
